d052acb75542392244b7f55769d777ed366190b2
Paulo Silva <silva.pff@gmail.com>
2020-05-10 19:08:13 +0100

    Criação da Classe Department

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Department d = new Department(1,"Informática");
+		System.out.println(d);
+
+	}
+
+}
