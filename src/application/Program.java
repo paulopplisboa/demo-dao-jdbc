dabf06861950697b9312bd71eb45aef12889e4d8
Paulo Silva <silva.pff@gmail.com>
2020-05-11 20:36:45 +0100

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,40 +11,48 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		//Department dep = new Department(1, "Informática");
-		//System.out.println(dep);
-		//Seller seller = new Seller(21, "Bobo", "bobo@gmail.com", new Date("20/06/1974"), 3000.0, dep);
-		//System.out.println(seller);
-		
+
+		// SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		// Department dep = new Department(1, "Informática");
+		// System.out.println(dep);
+		// Seller seller = new Seller(21, "Bobo", "bobo@gmail.com", new
+		// Date("20/06/1974"), 3000.0, dep);
+		// System.out.println(seller);
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("Teste 1 : Seller findById ");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n Test 2 : Seller findByDepartment ==== ");
-		Department department = new Department (2,null);
+		Department department = new Department(2, null);
 		List<Seller> lista = sellerDao.findByDepartment(department);
-		
+
 		for (Seller obj : lista) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n Test 3 : Seller findAll ==== ");
 		lista = sellerDao.findAll();
-		
+
 		for (Seller obj : lista) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n Test 4 : Seller insert ==== ");
-		
-		Seller seller1 = new Seller (null,"António", "antonio@gmail.com", new Date(),2500.0, department ); 
+
+		Seller seller1 = new Seller(null, "António", "antonio@gmail.com", new Date(), 2500.0, department);
 		sellerDao.insert(seller1);
-		System.out.println("Inserted! new id  = "+seller1.getId() );
+		System.out.println("Inserted! new id  = " + seller1.getId());
+
+		System.out.println("\n Test 5 : Seller Update ==== ");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Miranda");
+		sellerDao.update(seller);
+		System.out.println("Update complecto ");
 		
-				}
+
+	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -39,9 +39,9 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 
-			int rowsAffeted = st.executeUpdate();
-			if (rowsAffeted > 0) {
+			int rowsAffected = st.executeUpdate();
 
+			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
 					int id = rs.getInt(1);
@@ -49,9 +49,7 @@ public class SellerDaoJDBC implements SellerDao {
 				}
 				DB.closeResultSet(rs);
 			} else {
-
-				throw new DbException("Nenhuma linha afectada ");
-
+				throw new DbException("Unexpected error! No rows affected!");
 			}
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
@@ -62,8 +60,30 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
 
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+	
 	}
 
 	@Override
