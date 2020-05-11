d2bd1f4fe28bf1103fb4b9791617de3eddb8d80e
Paulo Silva <silva.pff@gmail.com>
2020-05-11 14:22:29 +0100

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -18,12 +17,19 @@ public class Program {
 		//Seller seller = new Seller(21, "Bobo", "bobo@gmail.com", new Date("20/06/1974"), 3000.0, dep);
 		//System.out.println(seller);
 		
-		SellerDao sellerDao = DaoFactory.creatSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("Teste 1 : Seller findById ");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-	}
+		System.out.println("\n Test 2 : Seller findByDepartment ==== ");
+		Department department = new Department (2,null);
+		List<Seller> lista = sellerDao.findByDepartment(department);
+		
+		for (Seller obj : lista) {
+			System.out.println(obj);
+		}
+				}
 
 }
